5652846a7d553a1cb92451ffda25ec1c6836df1c
richjack888 <angel800114@gmail.com>
2021-01-28 23:56:55 +0800

    Day30
    Signed-off-by: richjack888 <angel800114@gmail.com>

--- chapter8/src/array.java
+++ chapter8/src/array.java
@@ -1,18 +1,40 @@
+import java.util.Scanner;
+
 public class array {
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int[] myIntArray = new int[5];
+        int[] myIntegers = getIntegers(3);
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element " + i + ", type value was " + myIntegers[i]);
+        }
+        System.out.println("The average is " + getAverage(myIntegers));
 
-        for (int i = 0; i < myIntArray.length; i++) {
-            myIntArray[i] = i * 10;
+    }
+
+    private static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer value.\r");
+        int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
         }
 
-        for (int i = 0; i < myIntArray.length; i++) {
-            System.out.println("Element " + i + ", value is: " + myIntArray[i]);
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
 
         }
-
-
+        return sum / array.length;
     }
 
 
 }
+
+
+
