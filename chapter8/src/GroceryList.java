803a359ca6d330e627414a478cce5fb3e28f6bbf
richjack888 <angel800114@gmail.com>
2021-02-11 22:55:04 +0800

    Day39
    Signed-off-by: richjack888 <angel800114@gmail.com>

--- chapter8/src/GroceryList.java
+++ chapter8/src/GroceryList.java
@@ -1,15 +1,20 @@
 import java.util.ArrayList;
 
 public class GroceryList {
-
-    private ArrayList<String> groceryList = new ArrayList<String>();
+    private ArrayList<String> groceryList = new ArrayList();
 
     public static void main(String[] args) {
 
     }
 
-    public void addGroceryIem(String item) {
-        System.out.println("You have " + groceryList);
+
+    public void addGroceryItem(String item) {
+        System.out.println("You add - " + item + " - to grocery list");
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list.");
         for (int i = 0; i < groceryList.size(); i++) {
             System.out.println((i + 1) + ". " + groceryList.get(i));
         }
@@ -21,8 +26,17 @@ public class GroceryList {
     }
 
     public void removeGroceryItem(int position) {
-        String theItem = groceryList.get(position);
-        groceryList.remove(position);
+        String theItem = groceryList.get(position - 1);
+        groceryList.remove(position - 1);
+        System.out.println("item: " + theItem + " had been removed.");
+    }
+
+    public String findItem(String searchItem) {
+        int position = groceryList.indexOf(searchItem);
+        if (position >= 0) {
+            return groceryList.get(position);
+        }
+        return null;
     }
 
 }
