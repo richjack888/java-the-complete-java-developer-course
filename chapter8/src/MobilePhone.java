588bec48fafeb0b876324646854690a03d7a915d
richjack888 <angel800114@gmail.com>
2021-02-16 11:32:13 +0800

    Day41
    Signed-off-by: richjack888 <angel800114@gmail.com>

---
+++ chapter8/src/MobilePhone.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            System.out.println("Contact is already on file.");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(contactName)) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+}
