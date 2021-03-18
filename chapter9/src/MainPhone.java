e6d0804388a6b84610999b62657c32d9b40dabbc
RichJack888 <angel800114@gmail.com>
2021-03-18 23:55:51 +0800

    Day58

---
+++ chapter9/src/MainPhone.java
@@ -0,0 +1,9 @@
+public class MainPhone {
+    public static void main(String[] args) {
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
+    }
+}
