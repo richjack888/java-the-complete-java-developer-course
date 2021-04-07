b616cdf6d4d55941c66553e83ff06e1dfe1150b0
RichJack888 <angel800114@gmail.com>
2021-04-07 21:02:09 +0800

    Day71

--- chapter9/src/MainMyLinkedList.java
+++ chapter9/src/MainMyLinkedList.java
@@ -33,14 +33,29 @@ public class MainMyLinkedList {
 //        list.traverse(list.getRoot());
 
         SearchTree list = new SearchTree(null);
-        list.traverse(list.getRoot());
-//        String stringData = "7 5 3 9 1 2 8 0 4 6";
-        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        String stringData = "7 5 3 9 1 2 8 0 4 6";
+//        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
         String[] data = stringData.split(" ");
         for (String s : data) {
             list.addItem(new Node(s));
         }
+
+        list.removeItem(new Node("3"));
+        list.removeItem(new Node("2"));
+        list.removeItem(new Node("1"));
+        list.removeItem(new Node("0"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("6"));
+        list.removeItem(new Node("4"));
+        list.removeItem(new Node("5"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("7"));
+        list.removeItem(new Node("8"));
+        list.removeItem(new Node("9"));
         list.traverse(list.getRoot());
 
     }
 }
+
