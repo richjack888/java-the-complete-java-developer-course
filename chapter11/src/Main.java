7e23ad4fce430e388c231def0d03d7c44252ba3a
RichJack888 <angel800114@gmail.com>
2021-04-13 22:56:55 +0800

    Day75

--- chapter11/src/Main.java
+++ chapter11/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Container<Integer> obj = new Container<>();
+        obj.demo(new ArrayList<Number>());
+//        obj.demo(new ArrayList<Double>()); // won't work
+
+        Container<Number> obj1 = new Container<>();
+        obj1.demo2(new ArrayList<Integer>());
+        obj1.demo2(new ArrayList<Double>());
+
+    }
+}
+
+class Container<T extends Number> {
+
+    public void demo(ArrayList<? super T> obj) {
+        System.out.println("demo");
+    }
+
+    public void demo2(ArrayList<? extends T> obj) {
+        System.out.println("d22222222");
     }
 }
