dad36be801d5fd847d416ce7db6ed80a0eebb98e
RichJack888 <angel800114@gmail.com>
2021-04-19 20:17:25 +0800

    Day79

--- chapter11/src/MainScopeCheck.java
+++ chapter11/src/MainScopeCheck.java
@@ -1,12 +1,19 @@
 public class MainScopeCheck {
     public static void main(String[] args) {
-        String privateVar = " this is private to main()";
+        String varFour = "this is private to main()";
 
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println(" scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        ;
-        System.out.println(privateVar);
+        scopeInstance.userInner();
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        System.out.println("varThree is not accessible here " + scopeInstance.varThree);
 
-        scopeInstance.timesTwo();
+//        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+//        System.out.println(varFour);
+//        scopeInstance.timesTwo();
+//
+//        System.out.println("**************");
+//        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        innerClass.timesTwo();
+
     }
 }
--- chapter11/src/ScopeCheck.java
+++ chapter11/src/ScopeCheck.java
@@ -1,19 +1,44 @@
 public class ScopeCheck {
     public int publicVar = 0;
-    private int privateVar = 1;
+    private int varOne = 1;
 
     public ScopeCheck() {
-        System.out.println("ScopeCheck created, publicVar = " + this.publicVar + " : privateVar = " + this.privateVar);
+        System.out.println("ScopeCheck created, publicVar = " + this.publicVar + " , varOne = " + this.varOne);
     }
 
-    public int getPrivateVar() {
-        return this.privateVar;
+    public int getVarOne() {
+        return varOne;
     }
 
     public void timesTwo() {
-        int privateVar = 2;
+        int varTwo = 2;
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + " times two is " + i * privateVar);
+            System.out.println(i + " times two is " + i * varTwo);
         }
+
     }
+
+    public void userInner() {
+        InnerClass innerClass = new InnerClass();
+        System.out.println("VarThree from outer class: " + innerClass.varThree);
+    }
+
+    public class InnerClass {
+        private int varThree = 3;
+
+        public InnerClass() {
+            System.out.println("Inner Class created, varOne is " + varOne + " varThree is " + varThree);
+        }
+
+        public void timesTwo() {
+            System.out.println("varOne is still available here " + varOne);
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i + " times two is " + i * varThree);
+            }
+
+        }
+
+
+    }
+
 }
