59f8fd916a2e1848faf32524e3eccdd5807a2aa5
RichJack888 <angel800114@gmail.com>
2021-04-21 22:10:46 +0800

    Day81

--- chapter11/src/Main.java
+++ chapter11/src/Main.java
@@ -1,7 +1,47 @@
 public class Main {
     public static void main(String[] args) {
+        Wall wall = new Wall(10, 20);
+        System.out.println(wall.getHeight());
+        System.out.println(wall.getWidth());
+    }
+}
+
+class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+        this(0, 0);
+    }
+
+    public Wall(double width, double height) {
+
+        setWidth(width);
+        setHeight(height);
 
     }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width < 0 ? 0 : width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+
 }
 
 
