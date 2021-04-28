377c1f34ab82ece11736b5124336d48933d6a0be
RichJack888 <angel800114@gmail.com>
2021-04-28 22:49:49 +0800

    Day85

--- chapter12/src/MainTheatre.java
+++ chapter12/src/MainTheatre.java
@@ -1,17 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MainTheatre {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Muzha Guangming", 8, 10);
-//        theatre.getSeats();
-        if (theatre.reserveSeat("E05")) {
-            System.out.println("Okay, this seat is yours, one seat is $1");
-        } else {
-            System.out.println("Sorry, this seat is sold out already.");
+        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+//        printList(seatCopy);
+//
+//        seatCopy.get(1).reserve();
+//        if(theatre.reserveSeat("A02")){
+//            System.out.println("Please pay for A02");
+//        }else{
+//            System.out.println("Seat already reserved.");
+//        }
+
+        Collections.reverse(seatCopy);
+        System.out.println("Print seatCopy");
+        ;
+        printList(seatCopy);
+        System.out.println("Print original seat");
+        printList(theatre.seats);
+
+
+    }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
         }
-        if (theatre.reserveSeat("E05")) {
-            System.out.println("Okay, this seat is yours, one seat is $1");
-        } else {
-            System.out.println("Sorry, this seat is sold out already.");
-        }
+
+        System.out.println();
+        System.out.println("================================");
     }
 
 }
