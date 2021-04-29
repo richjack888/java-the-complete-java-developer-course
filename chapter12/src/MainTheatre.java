c663087c6751c838f80d807254e4f8a1a07e6183
RichJack888 <angel800114@gmail.com>
2021-04-29 22:13:06 +0800

    Day87

--- chapter12/src/MainTheatre.java
+++ chapter12/src/MainTheatre.java
@@ -6,22 +6,11 @@ public class MainTheatre {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Muzha Guangming", 8, 10);
         List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
-//        printList(seatCopy);
-//
-//        seatCopy.get(1).reserve();
-//        if(theatre.reserveSeat("A02")){
-//            System.out.println("Please pay for A02");
-//        }else{
-//            System.out.println("Seat already reserved.");
-//        }
 
-        Collections.reverse(seatCopy);
-        System.out.println("Print seatCopy");
-        ;
+        Collections.shuffle(seatCopy);
+        sortList(seatCopy);
+        System.out.println("Printing sorted seat seat copy");
         printList(seatCopy);
-        System.out.println("Print original seat");
-        printList(theatre.seats);
-
 
     }
 
@@ -33,5 +22,16 @@ public class MainTheatre {
         System.out.println();
         System.out.println("================================");
     }
+
+    public static void sortList(List<? extends Theatre.Seat> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
+    }
+
 
 }
