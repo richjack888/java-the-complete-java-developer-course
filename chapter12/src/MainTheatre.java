b73c0c3ccf6c6c86ddd8d5f64c3859442d3fa7bc
RichJack888 <angel800114@gmail.com>
2021-05-01 23:44:11 +0800

    Day89

--- chapter12/src/MainTheatre.java
+++ chapter12/src/MainTheatre.java
@@ -6,34 +6,18 @@ public class MainTheatre {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Muzha Guangming", 8, 12);
 
-//        if (theatre.reserveSeat("D12")) {
-//            System.out.println("Okay, this seat is yours");
-//        } else {
-//            System.out.println("Sorry, this seat is sold out already.");
-//        }
-//
-//        if (theatre.reserveSeat("D12")) {
-//            System.out.println("Okay, this seat is yours");
-//        } else {
-//            System.out.println("Sorry, this seat is sold out already.");
-//        }
-//
-//        if (theatre.reserveSeat("B13")) {
-//            System.out.println("Okay, this seat is yours");
-//        } else {
-//            System.out.println("Sorry, this seat is sold out already.");
-//        }
-
-        List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
-        Collections.reverse(reverseSeats);
-        printList(reverseSeats);
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00", 5));
+        priceSeats.add(theatre.new Seat("A00", 5));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+        printList(priceSeats);
 
 
     }
 
     public static void printList(List<Theatre.Seat> list) {
         for (Theatre.Seat seat : list) {
-            System.out.print(" " + seat.getSeatNumber() + " " + seat.getPrice());
+            System.out.print(seat.getSeatNumber() + ":$" + seat.getPrice() + ", ");
         }
 
         System.out.println();
