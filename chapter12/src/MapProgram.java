c989e87ced436f5b0ca758063a29c6dcaa15e2d7
RichJack888 <angel800114@gmail.com>
2021-05-06 22:38:40 +0800

    Day91

--- chapter12/src/MapProgram.java
+++ chapter12/src/MapProgram.java
@@ -4,21 +4,21 @@ import java.util.Map;
 public class MapProgram {
     public static void main(String[] args) {
         Map<String, String> languages = new HashMap<>();
-        if (languages.containsKey("Java")) {
-            System.out.println("Java already exists");
-        } else {
-            languages.put("Java", " a compiled high level, object-oriented, platform independent language.");
-            System.out.println("Java added successfully");
-        }
+
+        languages.put("Java", " a compiled high level, object-oriented, platform independent language.");
         languages.put("Python", " an interpreted, object-oriented, high-level programming language.");
         languages.put("Algol", " an algorithmic language");
         languages.put("BASIC", " Beginners All Purposes Symbolic Instruction Code.");
         languages.put("Lisp", " Therein lies madness");
 
-        if (languages.containsKey("Java")) {
-            System.out.println("Java is already in the map.");
+        if (languages.replace("Lisp", " Therein lies madness", "a functional programming language with imperative features.")) {
+            System.out.println("List replace");
         } else {
-            System.out.println(languages.put("Java", " this course is about Java"));
+            System.out.println("Lisp was not replaced");
+        }
+
+        for (String key : languages.keySet()) {
+            System.out.println(key + " : " + languages.get(key));
         }
 
     }
