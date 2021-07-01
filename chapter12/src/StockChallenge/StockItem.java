2dbb36266eb2824e21aadb64968a850e4214c468
richjack888 <angel800114@gmail.com>
2021-07-01 20:10:42 +0800

    Day115

---
+++ chapter12/src/StockChallenge/StockItem.java
@@ -0,0 +1,85 @@
+package StockChallenge;
+
+public class StockItem implements Comparable<StockItem> {
+    private final String name;
+    private double price;
+    private int quantityStock = 0;
+    private int reserved;
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0; // or here ( but you wouldn't normally do both)
+        this.reserved = 0;
+    }
+
+    public StockItem(String name, double price, int quantityStock) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = quantityStock;
+        this.reserved = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0.0) {
+            this.price = price;
+        }
+    }
+
+    public int quantityInStock() {
+        return quantityStock;
+    }
+
+    public void adjustStock(int quantity) {
+        int newQuantity = this.quantityStock + quantity;
+        if (newQuantity >= 0) {
+            this.quantityStock = newQuantity;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 17;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("Call Stock.StockItem.equals()");
+        if (obj == this) {
+            return true;
+        }
+
+        if ((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+
+        String objName = ((StockItem) obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int compareTo(StockItem o) {
+        System.out.println("Call Stock.StockItem.compareTo()");
+        if (this == o) {
+            return 0;
+        }
+        if (o != null) {
+            return this.name.compareTo(o.getName());
+        }
+
+        throw new NullPointerException();
+    }
+
+    @Override
+    public String toString() {
+        return name + " : $" + price;
+    }
+}
---
+++ chapter12/src/StockChallenge/StockList.java
@@ -0,0 +1,69 @@
+package StockChallenge;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new LinkedHashMap<>();
+    }
+
+    public int addStock(StockItem stockItem) {
+        if (stockItem != null) {
+            // check if already have stockItem, jf get null just pass stockItem of parameter
+            StockItem inStock = list.getOrDefault(stockItem.getName(), stockItem);
+            // if there are already stocks on this item, adjust the quantity
+            if (inStock != stockItem) {
+                stockItem.adjustStock(inStock.quantityInStock());
+            }
+            list.put(stockItem.getName(), stockItem);
+            return stockItem.quantityInStock();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity) {
+        StockItem inStock = list.getOrDefault(item, null);
+        if ((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0)) {
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+    public StockItem getStockItem(String key) {
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> Items() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    public Map<String, Double> PriceList() {
+        Map<String, Double> prices = new LinkedHashMap<>();
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            prices.put(item.getKey(), item.getValue().getPrice());
+        }
+        return Collections.unmodifiableMap(prices);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\nStock List\n";
+        double totalCost = 0.0;
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            StockItem stockItem = item.getValue();
+
+            double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+
+            s = s + stockItem + "   There are " + stockItem.quantityInStock() + " items in stock. Total value of items: ";
+            s = s + String.format("%.2f", itemValue) + "\n";
+            totalCost += itemValue;
+        }
+
+        return s + "    Total stock value " + String.format("%.2f", totalCost);
+    }
+}
