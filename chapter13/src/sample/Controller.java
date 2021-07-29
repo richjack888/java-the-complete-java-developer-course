1133224619a12714e9b01b41ca7b85a769991d83
richjack888 <angel800114@gmail.com>
2021-07-29 11:18:55 +0800

    Day133

--- chapter13/src/sample/Controller.java
+++ chapter13/src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 
@@ -9,8 +10,34 @@ public class Controller {
     private TextField nameField;
 
     @FXML
+    private Button ClickButton;
+
+    @FXML
+    private Button LeaveButton;
+
+    @FXML
+    public  void  initialize(){
+        ClickButton.setDisable(true);
+        LeaveButton.setDisable(true);
+    }
+
+    @FXML
     public void onButtonClicked(ActionEvent e) {
-        System.out.println("Are you typing: " + nameField.getText());
-        System.out.println("The following button was pressed: " + e.getSource());
+        String text = "You typing: " + nameField.getText();
+        if(e.getSource().equals(ClickButton)){
+            System.out.println("This is click button. " + text);
+        }else if(e.getSource().equals(LeaveButton)){
+            System.out.println("This is leave button. " + text);
+        }else {
+            System.out.println("None of above button. " + text);
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased(){
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty(); // second condition is not allow entering space
+        ClickButton.setDisable(disableButtons);
+        LeaveButton.setDisable(disableButtons);
     }
 }
