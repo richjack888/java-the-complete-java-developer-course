19d589ec76d1b1e677e52feb333f60b471fcd86e
richjack888 <angel800114@gmail.com>
2021-07-30 19:46:14 +0800

    Day134

--- chapter13/src/sample/Controller.java
+++ chapter13/src/sample/Controller.java
@@ -2,21 +2,22 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 
 public class Controller {
     @FXML
     private TextField nameField;
-
     @FXML
     private Button ClickButton;
-
     @FXML
     private Button LeaveButton;
+    @FXML
+    private CheckBox ourCheckBox;
 
     @FXML
-    public  void  initialize(){
+    public void initialize() {
         ClickButton.setDisable(true);
         LeaveButton.setDisable(true);
     }
@@ -24,20 +25,37 @@ public class Controller {
     @FXML
     public void onButtonClicked(ActionEvent e) {
         String text = "You typing: " + nameField.getText();
-        if(e.getSource().equals(ClickButton)){
+        if (e.getSource().equals(ClickButton)) {
             System.out.println("This is click button. " + text);
-        }else if(e.getSource().equals(LeaveButton)){
+        } else if (e.getSource().equals(LeaveButton)) {
             System.out.println("This is leave button. " + text);
-        }else {
+        } else {
             System.out.println("None of above button. " + text);
+        }
+
+//        try {
+//            Thread.sleep(10000);
+//        }catch (InterruptedException event){
+//            // we don't care about this
+//        }
+
+        if (ourCheckBox.isSelected()) {
+            nameField.clear();
+            ClickButton.setDisable(true);
+            LeaveButton.setDisable(true);
         }
     }
 
     @FXML
-    public void handleKeyReleased(){
+    public void handleKeyReleased() {
         String text = nameField.getText();
         boolean disableButtons = text.isEmpty() || text.trim().isEmpty(); // second condition is not allow entering space
         ClickButton.setDisable(disableButtons);
         LeaveButton.setDisable(disableButtons);
+    }
+
+    @FXML
+    private void handleChange() {
+        System.out.println("The checkbox is " + (ourCheckBox.isSelected() ? " checked " : " not checked "));
     }
 }
--- chapter13/src/sample/Main.java
+++ chapter13/src/sample/Main.java
@@ -2,14 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
