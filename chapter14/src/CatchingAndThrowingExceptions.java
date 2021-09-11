10cb60a864e2145aac57516679e5c9579da019a7
richjack888 <angel800114@gmail.com>
2021-09-11 17:18:25 +0800

    Day164

--- chapter14/src/CatchingAndThrowingExceptions.java
+++ chapter14/src/CatchingAndThrowingExceptions.java
@@ -4,34 +4,31 @@ import java.util.Scanner;
 
 public class CatchingAndThrowingExceptions {
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println("x / y = " + result);
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException | NoSuchElementException e) {
+            System.out.println(e);
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
     }
 
     private static int divide() {
         int x, y;
-        try {
-            x = getInt();
-            y = getInt();
-        } catch (NoSuchElementException e) {
-            throw new ArithmeticException("Fuck off! I need an Int");
-        }
+        x = getInt();
+        y = getInt();
         System.out.println("x is " + x + ", y is " + y);
-        try {
-            return x / y;
-        } catch (ArithmeticException e) {
-            throw new ArithmeticException("Stupid! u can't divide by 0 ");
-        }
+        return x / y;
     }
 
     private static int getInt() {
         Scanner s = new Scanner(System.in);
-        System.out.print("Please enter an integer: ");
+        System.out.println("Please enter an integer ");
         while (true) {
             try {
                 return s.nextInt();
             } catch (InputMismatchException e) {
-                // go round again. Read past the end of line in the input first
+                // go round again.  Read past the end of line in the input first
                 s.nextLine();
                 System.out.println("Please enter a number using only the digits 0 to 9 ");
             }
