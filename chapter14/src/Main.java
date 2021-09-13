c747425e1fcded8715c042413d2ffce049ef9da5
richjack888 <angel800114@gmail.com>
2021-09-13 17:37:44 +0800

    Day165

--- chapter14/src/Main.java
+++ chapter14/src/Main.java
@@ -1,5 +1,53 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        // enter some code
+        // test
     }
+}
+
+interface IPest {
+    void BeAnnoying();
+}
+
+class HouseFly  implements IPest {
+    void FlyAroundYourHead(){
+        System.out.println("FlyAroundYourHead");
+    }
+    void LandOnThings(){
+        System.out.println("LandOnThings");
+    }
+
+    public void BeAnnoying() {
+        FlyAroundYourHead();
+        LandOnThings();
+    }
+}
+
+class Telemarketer  implements IPest {
+    void CallDuringDinner(){
+        System.out.println("CallDuringDinner");
+    }
+    void ContinueTalkingWhenYouSayNo(){
+        System.out.println("ContinueTalkingWhenYouSayNo");
+    }
+
+    public void BeAnnoying() {
+        CallDuringDinner();
+        ContinueTalkingWhenYouSayNo();
+    }
+}
+
+class DiningRoom {
+
+//    DiningRoom(IPest[] pests) {
+//        pests = new HouseFly();
+//    }
+//
+//    void ServeDinner() {
+//        System.out.println("eating");
+//
+//        foreach pest in pests
+//        pest.BeAnnoying();
+//    }
 }
