768a00813c7c9bfcf21d94d661a56c96d503f3be
richjack888 <angel800114@gmail.com>
2021-10-05 17:18:37 +0800

    Day183

--- chapter14/src/introduceIo/randomAccessFile/Locations.java
+++ chapter14/src/introduceIo/randomAccessFile/Locations.java
@@ -6,93 +6,89 @@ import java.util.*;
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<>();
     private static Map<Integer, IndexRecord> index = new LinkedHashMap<>();
+    private static RandomAccessFile raIn;
 
     public static void main(String[] args) throws IOException {
 
-        try (RandomAccessFile raf = new RandomAccessFile("src/introduceIo/randomAccessFile/locations_raf.dat", "rwd")) {
+        try (RandomAccessFile raOut = new RandomAccessFile("src/introduceIo/randomAccessFile/locations_raf.dat", "rwd")) {
 
-            // The whole contents were made of  A part, B part, C part, data of locations.
+            // The whole contents were consisted of  A part, B part, C part, data of locations.
             // A part 0 ~ 3 byte
             // B part 4 ~ 7 byte
             // C part 8 ~ 1699 bytes
             // data of locations 1700 ~ end
 
             // A part - total number of locations which is 141
-            raf.writeInt(locations.size());
+            raOut.writeInt(locations.size());
 
-            // B part - start place of location data, which is after A + B + C part
+            // B part - start place of location's data, which is after A + B + C part
             int indexSize = locations.size() * 3 * Integer.BYTES; // length of C part
-            int locationStart = (int) (indexSize + raf.getFilePointer() + Integer.BYTES); // length of A + B + C part
-            raf.writeInt(locationStart);
+            int locationStart = (int) (indexSize + raOut.getFilePointer() + Integer.BYTES); // length of A + B + C part
+            System.out.println("locationStart: " + locationStart + ", which is the place of location's data start.");
+            raOut.writeInt(locationStart);
 
             // record place of C part start
-            long indexStart = raf.getFilePointer();
-            System.out.println("index Start: "  + indexStart + ", which is the place of C part start.");
+            long indexStart = raOut.getFilePointer();
+            System.out.println("indexStart " + indexStart + ", which is the place of C part start.");
 
-            // measure every location's length and write all data of locations
+            // measure every location's length and write all location's data
             int startPointer = locationStart;
-            System.out.println("start pointer: " + startPointer + ", which is the place of location data start.");
-            raf.seek(startPointer); // jump to place of location data start
+            raOut.seek(startPointer); // jump to place of location's data start
 
             for (Location location : locations.values()) {
-                raf.writeInt(location.getLocationID());
-                raf.writeUTF(location.getDescription());
+                raOut.writeInt(location.getLocationID());
+                raOut.writeUTF(location.getDescription());
 
                 // retrieve Map of exits data (Map<String, Integer> exits)
                 StringBuilder stringBuilder = new StringBuilder();
                 for (String direction : location.getExits().keySet()) {
                     if (!direction.equalsIgnoreCase("Q")) {
-                        stringBuilder.append(direction);
+                        stringBuilder.append(direction); // get exits key
                         stringBuilder.append(",");
-                        stringBuilder.append(location.getExits().get(direction));
+                        stringBuilder.append(location.getExits().get(direction)); // get exits value
                         stringBuilder.append(",");
+                        // would like W,2,E,3,S,1,....
                     }
                 }
-                raf.writeUTF(stringBuilder.toString());
+                raOut.writeUTF(stringBuilder.toString());
 
                 // calculate each of location length
-                int length = (int) (raf.getFilePointer() - startPointer);
+                int length = (int) (raOut.getFilePointer() - startPointer); // end of location - start of location
                 IndexRecord record = new IndexRecord(startPointer, length);
 
                 index.put(location.getLocationID(), record);  // put result to index map, so later C part could use it
-                startPointer = (int) raf.getFilePointer(); // when current location was recorded, move pointer to the end
+                startPointer = (int) raOut.getFilePointer(); // when current location was recorded, move pointer to the end of current location
             }
 
             // C part - index of location
-            raf.seek(indexStart); // back to C part start
+            raOut.seek(indexStart); // back to C part start
             for (Integer locationID : index.keySet()) {
-                raf.writeInt(locationID);
-                raf.writeInt(index.get(locationID).getStartByte());
-                raf.writeInt(index.get(locationID).getLength());
+                raOut.writeInt(locationID);
+                raOut.writeInt(index.get(locationID).getStartByte());
+                raOut.writeInt(index.get(locationID).getLength());
             }
         }
     }
 
     static {
+        try {
+            raIn = new RandomAccessFile("src/introduceIo/randomAccessFile/locations_raf.dat", "rwd");
+            int numLocations = raIn.readInt();
+            long locationStart = raIn.readInt();
 
-        try (ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(new FileInputStream("src/introduceIo/randomAccessFile/locations.dat")))) {
-            boolean eof = false;
-            while (!eof) {
-                try {
-                    Location location = (Location) locFile.readObject();
-                    System.out.println("Read location " + location.getLocationID() + " : " + location.getDescription());
-                    System.out.println("Found " + location.getExits().size() + " exits");
+            while (raIn.getFilePointer() < locationStart) {
+                int locationID = raIn.readInt();
+                int locationsStart = raIn.readInt();
+                int locationsLength = raIn.readInt();
 
-                    locations.put(location.getLocationID(), location);
-                } catch (EOFException e) {
-                    eof = true;
-                    System.out.println("EOF exception: " + e.getMessage());
-                }
+                IndexRecord record = new IndexRecord(locationsStart, locationsLength);
+                index.put(locationID, record);
             }
 
-        } catch (InvalidClassException e) {
-            System.out.println("InvalidClassException: " + e.getMessage());
-        } catch (IOException io) {
-            System.out.println("IO exception: " + io.getMessage());
-        } catch (ClassNotFoundException e) {
-            System.out.println("ClassNotFoundException: " + e.getMessage());
-        }
 
+        } catch (IOException e) {
+            System.out.println("IOException in static initializer " + e.getMessage());
+        }
 
     }
 
