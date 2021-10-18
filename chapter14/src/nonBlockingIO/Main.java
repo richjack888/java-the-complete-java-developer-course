1e40f8ca3172dd9a60922e95723f0ed3efd932b9
richjack888 <angel800114@gmail.com>
2021-10-18 15:58:22 +0800

    Day190

--- chapter14/src/nonBlockingIO/Main.java
+++ chapter14/src/nonBlockingIO/Main.java
@@ -7,20 +7,32 @@ import java.nio.channels.FileChannel;
 
 public class Main {
     public static void main(String[] args) {
-        try {
+//        try {
 //            Path dataPath = Paths.get("src/nonBlockingIO/data.txt");
 //            Files.write(dataPath, "\nLine 4".getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
 //            List<String> lines = Files.readAllLines(dataPath);
 //            for (String line : lines) {
 //                System.out.println(line);
 //            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-            FileOutputStream binFile = new FileOutputStream("src/nonBlockingIO/data.dat");
-            FileChannel binChannel = binFile.getChannel();
-            byte[] outputBytes = "Hello World".getBytes();
+
+        try (FileOutputStream binFile = new FileOutputStream("src/nonBlockingIO/data.dat");
+             FileChannel binChannel = binFile.getChannel()) {
+            byte[] outputBytes = "Hello World!".getBytes();
             ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
             int numBytes = binChannel.write(buffer);
             System.out.println("numBytes written was: " + numBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(666);
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
