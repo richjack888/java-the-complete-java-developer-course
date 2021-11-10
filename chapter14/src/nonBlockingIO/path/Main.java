76519dbc04be4f204610b93bbf22001a6d6c7b84
richjack888 <angel800114@gmail.com>
2021-11-10 12:09:57 +0800

    Day204

--- chapter14/src/nonBlockingIO/path/Main.java
+++ chapter14/src/nonBlockingIO/path/Main.java
@@ -9,14 +9,25 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault().getPath("src/nonBlockingIO/path/workingDirectory.txt");
+        Path path = FileSystems.getDefault().getPath("workingDirectory.txt");
         printFile(path);
-//        Path filePath = FileSystems.getDefault().getPath("src/nonBlockingIO/path/files","files.txt");
-        Path filePath = FileSystems.getDefault().getPath("src/nonBlockingIO/path/files/files.txt");
-        printFile(filePath);
 
-        filePath = Paths.get("C:\\software\\JetBrains\\IdeaProjects\\java-the-complete-java-developer-course\\outFileFromChapter14.txt");
-        printFile(filePath);
+//        Path filePath = FileSystems.getDefault().getPath("src/nonBlockingIO/path/files","files.txt");
+//        Path filePath = FileSystems.getDefault().getPath("src/nonBlockingIO/path/files/files.txt");
+//        printFile(filePath);
+//
+//        filePath = Paths.get("C:\\software\\JetBrains\\IdeaProjects\\java-the-complete-java-developer-course\\outFileFromChapter14.txt");
+//        printFile(filePath);
+
+        // check default path
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+        // check default path 2
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current absolute path is: " + s);
+
+
     }
 
     private static void printFile(Path path) {
