a58f5cdc486928227fe4dfb0dad0e1fb7f0d4656
richjack888 <angel800114@gmail.com>
2021-11-11 15:28:47 +0800

    Day205

--- chapter14/src/nonBlockingIO/path/Main.java
+++ chapter14/src/nonBlockingIO/path/Main.java
@@ -2,30 +2,33 @@ package nonBlockingIO.path;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault().getPath("workingDirectory.txt");
+
+//        Path path = FileSystems.getDefault().getPath("workingDirectory.txt");
+//        printFile(path);
+//
+////      Path filePath = FileSystems.getDefault().getPath("src/nonBlockingIO/path/files","files.txt");
+//        path = FileSystems.getDefault().getPath("src/nonBlockingIO/path/files/files.txt");
+//        printFile(path);
+//
+//        path = Paths.get("C:\\software\\JetBrains\\IdeaProjects\\java-the-complete-java-developer-course\\outFileFromChapter14.txt");
+//        printFile(path);
+
+        Path path = Paths.get("./src/nonBlockingIO/path","files", "files.txt");
         printFile(path);
 
-//        Path filePath = FileSystems.getDefault().getPath("src/nonBlockingIO/path/files","files.txt");
-//        Path filePath = FileSystems.getDefault().getPath("src/nonBlockingIO/path/files/files.txt");
-//        printFile(filePath);
+
+//        // check working directory path 1
+//        System.out.println("Working Directory = " + System.getProperty("user.dir"));
 //
-//        filePath = Paths.get("C:\\software\\JetBrains\\IdeaProjects\\java-the-complete-java-developer-course\\outFileFromChapter14.txt");
-//        printFile(filePath);
-
-        // check default path
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-
-        // check default path 2
-        Path currentRelativePath = Paths.get("");
-        String s = currentRelativePath.toAbsolutePath().toString();
-        System.out.println("Current absolute path is: " + s);
+//        // check working directory path 2
+//        Path currentPath = Paths.get("");
+//        System.out.println("Working Directory = " + currentPath.toAbsolutePath());
 
 
     }
