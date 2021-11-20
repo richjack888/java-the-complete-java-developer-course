49a859ce5fdc3d5ccb130294ec8e217abbefcadd
richjack888 <angel800114@gmail.com>
2021-11-20 11:39:24 +0800

    Day209

--- chapter14/src/nonBlockingIO/path/copyExamples/Main.java
+++ chapter14/src/nonBlockingIO/path/copyExamples/Main.java
@@ -8,11 +8,21 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) {
         try {
-            Path sourceFile = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples", "file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples", "file1copy.txt");
-            Files.copy(sourceFile, copyFile);
+//            Path sourceFile = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples", "file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples", "file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
+//            Path sourceFile = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples","Dir1");
+//            Path copyFile = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples","Dir4");
+//            Files.copy(
+
+            Path fileToMove = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples", "file1copy.txt");
+            Path destination = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples", "Dir1", "file1copy.txt");
+            Files.move(fileToMove, destination);
+
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println(e);
         }
     }
 }
