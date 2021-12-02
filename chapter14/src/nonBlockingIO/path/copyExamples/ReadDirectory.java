eca404160ede34b728250affd6f6e4ffb7ba3597
richjack888 <angel800114@gmail.com>
2021-12-02 11:05:37 +0800

    Day216

--- chapter14/src/nonBlockingIO/path/copyExamples/ReadDirectory.java
+++ chapter14/src/nonBlockingIO/path/copyExamples/ReadDirectory.java
@@ -6,7 +6,7 @@ import java.nio.file.*;
 public class ReadDirectory {
     public static void main(String[] args) {
         Path directory = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples/Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.dat")) {
             for (Path file : contents) {
                 System.out.println(file.getFileName());
             }
