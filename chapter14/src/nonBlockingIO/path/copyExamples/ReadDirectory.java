50999aac8c39bfde7a1f1bd37e8fc9179ebb138f
richjack888 <angel800114@gmail.com>
2021-12-04 21:28:09 +0800

    Day217

--- chapter14/src/nonBlockingIO/path/copyExamples/ReadDirectory.java
+++ chapter14/src/nonBlockingIO/path/copyExamples/ReadDirectory.java
@@ -5,8 +5,20 @@ import java.nio.file.*;
 
 public class ReadDirectory {
     public static void main(String[] args) {
+
+//        DirectoryStream.Filter<Path> filter =
+//                new DirectoryStream.Filter<Path>() {
+//                    @Override
+//                    public boolean accept(Path entry) throws IOException {
+//                        return (Files.isRegularFile(entry));
+//                    }
+//                };
+
+        DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
+
         Path directory = FileSystems.getDefault().getPath("src/nonBlockingIO/path/copyExamples/Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.dat")) {
+        //        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.dat")) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
             for (Path file : contents) {
                 System.out.println(file.getFileName());
             }
