13e692b8a865952e39e012746acb5eac07401a18
richjack888 <angel800114@gmail.com>
2021-12-11 11:44:32 +0800

    Day222

---
+++ chapter14/src/CopyFiles.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class CopyFiles extends SimpleFileVisitor<Path> {
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error accessing file: " + file.toAbsolutePath() + " " + exc.getMessage());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return super.preVisitDirectory(dir, attrs);
+    }
+}
