4c9e0c630605e33770ff2163cca469f220bbe13f
richjack888 <angel800114@gmail.com>
2021-12-14 17:05:18 +0800

    Day224

--- chapter14/src/SeparatorsAndTemp.java
+++ chapter14/src/SeparatorsAndTemp.java
@@ -1,8 +1,6 @@
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SeparatorsAndTemp {
 
@@ -51,16 +49,33 @@ public class SeparatorsAndTemp {
 //        }
 
 
-        System.out.println("--- Copy Dir2 to Dir4 / Dir2Copy ---");
-        Path copyPath = FileSystems.getDefault().getPath(String.join(File.separator, "src", "nonBlockingIO", "path", "copyExamples", "Dir4","Dir2Copy"));
-        // FileTree/Dir4/Dir2Copy
-        Path dir2Path = FileSystems.getDefault().getPath(String.join(File.separator, "src", "nonBlockingIO", "path", "copyExamples", "Dir2"));
-        try{
-            Files.walkFileTree(dir2Path,new CopyFiles(dir2Path,copyPath) );
-        }catch (IOException e){
-            System.out.println(e.getMessage());
-        }
+//        System.out.println("--- Copy Dir2 to Dir4 / Dir2Copy ---");
+////        Path copyPath =  Paths.get(String.join(File.separator, "src", "nonBlockingIO", "path", "copyExamples", "Dir4","Dir2Copy"));
+//        Path copyPath = FileSystems.getDefault().getPath(String.join(File.separator, "src", "nonBlockingIO", "path", "copyExamples", "Dir4","Dir2Copy"));
+//        // FileTree/Dir4/Dir2Copy
+////        Path dir2Path =  Paths.get(String.join(File.separator, "src", "nonBlockingIO", "path", "copyExamples", "Dir2"));
+//        Path dir2Path = FileSystems.getDefault().getPath(String.join(File.separator, "src", "nonBlockingIO", "path", "copyExamples", "Dir2"));
+//        try{
+//            Files.walkFileTree(dir2Path,new CopyFiles(dir2Path,copyPath) );
+//        }catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
 
+
+        File file = new File("/Examples/file.txt"); // C:\\Examples\file.txt
+        Path convertedPath = file.toPath();
+        System.out.println("converted path = " + convertedPath);
+
+        File parent = new File("/Examples");    // C:\\Examples
+        File resolvedFile = new File(parent, "dir/file.txt");   // dir\\file.txt
+        System.out.println(resolvedFile.toPath());
+
+        resolvedFile = new File("/Examples", "dir/file.txt");   // C;\\Examples  dir\\file.txt
+        System.out.println(resolvedFile);
+
+        Path parentPath = Paths.get("/Examples");   // C:\\Examples
+        Path childRelativePath = Paths.get("dir/file.txt"); // dir\\file.txt
+        System.out.println(parentPath.resolve(childRelativePath));
 
     }
 
