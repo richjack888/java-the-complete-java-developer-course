8eddfed40fa28c1b392e319c95fd57c7b58b426e
richjack888 <angel800114@gmail.com>
2021-12-31 17:32:35 +0800

    Day234

--- chapter15/src/Deadlocks.java
+++ chapter15/src/Deadlocks.java
@@ -1,7 +1,10 @@
 import java.util.Random;
 
-public class ProducerandConsumer {
+public class Deadlocks {
     public static void main(String[] args) {
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
 
     }
 }
@@ -12,18 +15,28 @@ class Message {
 
     public synchronized String read() {
         while (empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
 
+            }
         }
         empty = true;
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message) {
         while (!empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
 
+            }
         }
         empty = false;
         this.message = message;
+        notifyAll();
 
     }
 
@@ -56,6 +69,28 @@ class Writer implements Runnable {
             }
         }
         message.write("Finished");
+
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random ramdom = new Random();
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished");
+             latestMessage = message.read()) {
+            System.out.println(latestMessage);
+        }
+        try {
+            Thread.sleep(ramdom.nextInt(2000));
+        } catch (InterruptedException e) {
+
+        }
 
     }
 }
