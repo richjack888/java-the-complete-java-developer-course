a2e8097b0a7fa667eada604ed9bca6aad495b227
richjack888 <angel800114@gmail.com>
2022-01-05 23:17:37 +0800

    Day237

--- chapter15/src/ProducerConsumer.java
+++ chapter15/src/ProducerConsumer.java
@@ -4,12 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static EOF;
+
 public class ProducerConsumer {
+
+    public static final String EOF = "EOF";
+
     public static void main(String[] args) {
         List<String> buffer = new ArrayList<String>();
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN);
+
+        new Thread(producer).start();
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
 
     }
 }
