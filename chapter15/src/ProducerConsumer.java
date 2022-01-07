8a5da00359d3a4d3579224c6fc53db22fe7d86b3
richjack888 <angel800114@gmail.com>
2022-01-07 22:41:35 +0800

    Day238

--- chapter15/src/ProducerConsumer.java
+++ chapter15/src/ProducerConsumer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+// Don't know why can't import
 import static EOF;
 
 public class ProducerConsumer {
---
+++ chapter18/src/debuggerDemo/StringUtilities.java
@@ -0,0 +1,11 @@
+package debuggerDemo;
+
+public class StringUtilities {
+    private StringBuilder sBuilder = new StringBuilder();
+    private int charsAdded = 0;
+
+    public void addChar(StringBuilder sb, char c) {
+        this.sBuilder.append(c);
+        charsAdded++;
+    }
+}
