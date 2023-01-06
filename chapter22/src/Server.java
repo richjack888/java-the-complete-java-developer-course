7debf5ebf1e31ff38cfa9c63155ebf332a050317
richjack888 <angel800114@gmail.com>
2023-01-06 22:43:35 +0800

    Day274

--- chapter22/src/Server.java
+++ chapter22/src/Server.java
@@ -4,20 +4,28 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
+        System.out.println("Server start!");
         try (ServerSocket serverSocket = new ServerSocket(5688)) {
-            System.out.println("Server start!");
-            Socket socket = serverSocket.accept();
-            System.out.println("Sever was connected by client -> " + socket.getInetAddress() + ":" + socket.getPort());
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Sever was connected by client -> " + socket.getInetAddress() + ":" + socket.getPort());
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
                 String echo = input.readLine();
                 if (echo.equals("exit")) {
+
                     break;
                 }
                 output.println("Server receive your message: " + echo);
                 System.out.println("Message from client: " + echo);
+
+//                // todo no reaction
+//                if(socket.isClosed()){
+//                    System.out.println(socket.getInetAddress() + ":" + socket.getPort() + " close connection.");
+//                }
+
+
             }
         } catch (IOException e) {
             System.out.println("Server exception: " + e.getMessage());
