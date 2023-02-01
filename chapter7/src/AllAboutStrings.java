b067e8811223a1347d5c1d63b1589cf639446826
richjack888 <angel800114@gmail.com>
2023-02-01 21:59:43 +0800

    Day291

---
+++ chapter7/src/AllAboutStrings.java
@@ -0,0 +1,37 @@
+public class AllAboutStrings {
+    public static void main(String[] args) {
+//        printInformation("Hello World");
+//        printInformation("");
+//        printInformation("\t   \n");
+
+        String helloWorld = "Hello World";
+//        System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
+//        System.out.printf("index of World = %d %n", helloWorld.indexOf("World"));
+//
+//        System.out.printf("index of l = %d %n", helloWorld.indexOf('l'));
+//        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l'));
+//
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l',
+                3));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l',
+                8));
+    }
+
+    private static void printInformation(String string) {
+        int length = string.length();
+        System.out.printf("Length = %d %n", length);
+
+        if (string.isEmpty()) {
+            System.out.println("String is Empty");
+            return;
+        }
+
+        if (string.isBlank()) {
+            System.out.println("String is Blank");
+        }
+
+        System.out.printf("First char = %c %n", string.charAt(0));
+
+        System.out.printf("Last char = %c %n", string.charAt(length - 1));
+    }
+}
